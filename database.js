ee1699ea6fa5af40a7da88a93674a4594791b350
cryptit <74343532+cryptit@users.noreply.github.com>
2020-11-13 11:08:32 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,46 +1,20 @@
-
-const mysql = require('mysql');
-
+const dbconn = require('./dbconn');
 
 function execQuery(sql, placeholderValues, callback) {
-	let con = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '12345678',
-		database: 'blog'
-	});
-	con.connect(err => {
-		if (err) {
-			throw err;
-			console.log(err);
-			console.error('DB connection error!');
-		}
-	});	
-	con.query(sql, placeholderValues, callback);
-	con.end();	
+	dbconn.query(sql, placeholderValues, callback);
 }
 
 //Uses Promise; was having trouble with grabbing return values from callback; had to settle for async-await finally
 function execQuery2(sqlQuery, placeholderValues) {
-	return new Promise((resolve, reject) => {
-		 let con = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: '12345678',
-			database: 'blog'
-		});
-						
-		con.query(sqlQuery, placeholderValues, (err, result, fields) => {
+	return new Promise((resolve, reject) => {					
+		dbconn.query(sqlQuery, placeholderValues, (err, result, fields) => {
 			if (err) {
 				console.error(err);
-				reject('ERROR')
+				reject('ERROR');
 			}
-			con.end();
 			resolve(result);
 		});
 	});
 }
-// SOLVE UNHANDLED PROMISE ISSUE;
 
 module.exports = {execQuery, execQuery2};
-
